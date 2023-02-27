c9a3a0f4bc40c28b17629f5c1c2d4f6976e094c3
MAC <horlarmihleykan10@gmail.com>
2023-02-28 00:52:26 +0100

    new

---
+++ todo_app/__init__.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysegcret'
+
+########################   ####################
+
+        # SQL DATABASE AND MODELS
+
+##########################################
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://shabzy:4ugAtmMIpXIcTl7tFjmWjaPiFOIVtEbH@dpg-cfujslha6gdrs8jjq1ug-a.oregon-postgres.render.com/music_id63"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db,render_as_batch=True)
+
+bcrypt = Bcrypt(app)
+
+
+ma = Marshmallow(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+
+mail = Mail(app)
+
+
+from todo_app.users.views import users
+
+app.register_blueprint(users)
