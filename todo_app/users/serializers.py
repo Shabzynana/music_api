3660b761a4eaf05da4c649bcf35b907228719e97
Adebiyi Oluwafemi <horlarmihleykan10@gmail.com>
2023-03-09 00:48:42 +0100

    Delete todo_app directory

--- todo_app/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from todo_app import ma
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "email", "username", "first_name", "last_name", "gender")
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
